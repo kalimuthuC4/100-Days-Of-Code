83937bdf6be388a1e4ba7e16b8e9f2a9bcd8a2d2
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2022-01-18 19:47:26 +0530

    Add files via upload

---
+++ day 1/BandNameGenerater.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generater")
+city = input("which city did you grew up ?\n")
+pet = input("Enter the name of your pet:\n")
+print("Your Band Name :"+ city + " " + pet)
